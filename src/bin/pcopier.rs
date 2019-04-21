41bc922426ff0ffddf5ccaf976dfe1ebc1295919
Rob Day <rkd@rkd.me.uk>
2019-04-21 14:48:44 +0000

    Add --rm command

--- src/bin/pcopier.rs
+++ src/bin/pcopier.rs
@@ -10,7 +10,7 @@ use std::time::SystemTime;
 
 use clap::App;
 use std::path::Path;
-use std::fs::{create_dir_all, copy, DirEntry};
+use std::fs::{create_dir_all, copy, DirEntry, remove_file};
 use std::io;
 
 fn main() {
@@ -21,6 +21,7 @@ fn main() {
                           .args_from_usage(
                               "--input=<inputdir>  'Sets the source directory'
                                --output=<outputdir> 'Sets the output directory'
+                               --rm 'Remove files after copying (or if already copied)'
                                --dry-run      'Don't actually move files'")
                           .get_matches();
 
@@ -29,54 +30,66 @@ fn main() {
     let output = matches.value_of("output").unwrap();
     println!("Copying from {} to {}", input, output);
     let dry_run = matches.is_present("dry-run");
+    let remove = matches.is_present("rm");
     if dry_run {
         println!("Dry run only");
     }
 
     let input_path = Path::new(input);
     let output_path = Path::new(output);
-    match copy_photos(input_path, output_path, dry_run) {
+    match copy_photos(input_path, output_path, dry_run, remove) {
         Ok(num) => { println!("{} photos copied", num); },
         Err(e) => { println!("Failure: {}", e) }
     }
 }
 
-fn copy_photos(input_path: &Path, output_path: &Path, dry_run: bool) -> io::Result<u64> {
+fn copy_photos(input_path: &Path, output_path: &Path, dry_run: bool, remove: bool) -> io::Result<u64> {
     let mut copied = 0;
     let dir_entries: Vec<io::Result<DirEntry>> = input_path.read_dir()?.collect();
+    println!("{} files to process", dir_entries.len());
     for (num, entry) in dir_entries.iter().enumerate() {
         if let Ok(entry) = entry {
-            if let Some(fname) = entry.path().file_name() {
-                let unix_timestamp = entry.metadata().unwrap().created().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-                let ts = NaiveDateTime::from_timestamp(unix_timestamp as i64, 0);
-                let dated_output_dir = output_path.join(ts.year().to_string()).join(ts.month().to_string());
-                if ! dated_output_dir.exists() {
-                    if dry_run {
-                        println!("Not creating {} (dry run)", dated_output_dir.display());
-                    } else {
-                        create_dir_all(&dated_output_dir)?;
+            if entry.path().is_file() {
+                println!("Processing {}", entry.path().display());
+                if let Some(fname) = entry.path().file_name() {
+                    let unix_timestamp = entry.metadata().unwrap().modified().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+                    let ts = NaiveDateTime::from_timestamp(unix_timestamp as i64, 0);
+                    let dated_output_dir = output_path.join(ts.year().to_string()).join(ts.month().to_string());
+                    if ! dated_output_dir.exists() {
+                        if dry_run {
+                            println!("Not creating {} (dry run)", dated_output_dir.display());
+                        } else {
+                            create_dir_all(&dated_output_dir)?;
+                        }
                     }
-                }
-                let output_file = dated_output_dir.join(fname);
-                if dry_run {
-                    println!("Not copying {} to {} ({}/{}) (dry run)",
-                    entry.path().display(),
-                    output_file.display(),
-                    num+1,
-                    dir_entries.len());
-                } else {
-                    if output_file.exists() && output_file.metadata().unwrap().len() == entry.path().metadata().unwrap().len() {
-                        println!("{} already exists at the correct file size, not copying", output_file.display());
-                    } else {
-                        println!("Copying {} to {} ({}/{})",
+                    let output_file = dated_output_dir.join(fname);
+                    if dry_run {
+                        println!("Not copying {} to {} ({}/{}) (dry run)",
                         entry.path().display(),
                         output_file.display(),
                         num+1,
                         dir_entries.len());
-                        let result = copy(entry.path(), output_file);
-                        match result {
-                            Ok(_) => { copied += 1; },
-                            Err(e) => { println!("{} failed to copy: {}", entry.path().display(), e) },
+                    } else {
+                        if output_file.exists() {
+                            if output_file.metadata().unwrap().len() == entry.path().metadata().unwrap().len() {
+                                println!("{} already exists at the correct file size, not copying", output_file.display());
+                                if remove {
+                                    remove_file(entry.path())?;
+                                }
+                            } else {
+                                return Err(io::Error::new(io::ErrorKind::Other, format!("{} already exists but is different", output_file.display())));
+                            }
+                        } else {
+                            println!("Copying {} to {} ({}/{})",
+                            entry.path().display(),
+                            output_file.display(),
+                            num+1,
+                            dir_entries.len());
+                            copy(entry.path(), output_file)?;
+                            if remove {
+                                remove_file(entry.path())?;
+                            }
+                            copied += 1;
                         }
                     }
                 }
